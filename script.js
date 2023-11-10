842b2c04779fd7ad64944bb8af46c95b34668609
Anant kumar <anant1042005@gmail.com>
2023-11-10 23:32:34 +0530

    correct the design

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+function openSidebar() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("openBtn").style.display = "none";
+  document.querySelector(".content").classList.add("content-shifted");
+}
 
-      function openSidebar() {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("openBtn").style.display = "none";
-        document.querySelector(".content").classList.add("content-shifted");
-      }
-
-      function closeSidebar() {
-        document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("openBtn").style.display = "block";
-        document.querySelector(".content").classList.remove("content-shifted");
-      }
+function closeSidebar() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("openBtn").style.display = "block";
+  document.querySelector(".content").classList.remove("content-shifted");
+}
